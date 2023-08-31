0d7b4a95c51ee1f22ea3d274ae71bffd4d0b5888
Foyez-Ahamed <foyez1435@gmail.com>
2023-08-31 13:02:41 +0600

    project updated

--- js/universe.js
+++ js/universe.js
@@ -3,14 +3,22 @@ const loadUniverseHub = async () => {
     const data = await response.json();
     // console.log(data.data.tools[0]);
     const allUniverseHub = data.data.tools;
-    displayUniverseHub(allUniverseHub);
+    displayUniverseHub(allUniverseHub, false);
 
-}
+};
 
-const displayUniverseHub = universeHub => {
+const displayUniverseHub = (universeHub, isSort) => {
     const universeContainer =  document.getElementById('universe-container');
+
+    if(isSort){
+        universeHub = universeHub.sort((a, b) => {
+            return new Date(b.published_in) - new Date(a.published_in);
+        })
+    }
+    universeContainer.textContent = "";
+
     universeHub.forEach( universe => {
-        console.log(universe);
+        // console.log(universe);
         const allFeatures = universe.features.map((item, index) => {
             return `<li> ${index + 1} . ${item}</li>`;
         })
@@ -21,6 +29,7 @@ const displayUniverseHub = universeHub => {
 
         universeDiv.innerHTML = `
        
+        <div onclick="showDetailsByModal('${universe.id}')">
         <figure><img src="${universe?.image}" /></figure>
             <div class="card-body">
               <h2 class="card-title">Features</h2>
@@ -28,8 +37,8 @@ const displayUniverseHub = universeHub => {
               <p class = "text-xl font-bold"> ${universe?.name} </p>
               <p> ${universe?.published_in
               } </p>
-              
             </div>
+        </div>
         
         
         `
@@ -37,8 +46,20 @@ const displayUniverseHub = universeHub => {
     });
 }
 
-const showDetailsByModal = () =>{
-    console.log('Hello');
+const sortDate = async () =>  {
+    const response = await fetch('https://openapi.programming-hero.com/api/ai/tools');
+    const data = await response.json();
+    // console.log(data.data.tools[0]);
+    const allUniverseHub = data.data.tools;
+    displayUniverseHub(allUniverseHub, true);
+}
+
+const showDetailsByModal = async (id) => {
+    my_modal_5.showModal();
+    const response = await fetch(`https://openapi.programming-hero.com/api/ai/tool/${id}`);
+    const data = await response.json();
+    console.log(data.data);
+
 }
 
 loadUniverseHub();
